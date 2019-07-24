9fa9c58752418ae438f78fa6f16c0b7943aa36bc
jerodb <jero.db@live.com>
2019-07-24 01:18:44 -0300

    cleaner config file

--- config.js
+++ config.js
@@ -10,26 +10,20 @@ const {
   MYSQL_DB, MYSQL_USER, MYSQL_PASS, MYSQL_HOST, MYSQL_PORT
 } = process.env
 
-let CONFIG = {
+const CONFIG = {
   BASE_URL: BASE_URL || '/',
   ENV: NODE_ENV || 'development',
   HOST: HOST || '',
   IMAGES_URL: `${HOST}${BASE_URL}images/`,
   PORT: PORT || '3000',
-  RECAPTCHA_KEY
-}
+  RECAPTCHA_KEY,
 
-CONFIG = {
-  ...CONFIG,
   AUTH_CLIENT_ID,
   AUTH_DOMAIN,
   AUTH_RESPONSE_TYPE,
   AUTH_REDIRECT_URI,
   AUTH_SCOPE,
-}
 
-CONFIG = {
-  ...CONFIG,
   MYSQL_DB,
   MYSQL_USER,
   MYSQL_PASS,
