050a7b5da3e920fee594b907364a27e8c7a72124
jerodb <jero.db@live.com>
2019-07-26 19:06:44 -0300

    /api/user/set changed to /api/set/user

--- src/services/users.js
+++ src/services/users.js
@@ -1,7 +1,7 @@
 /* import axios from 'axios'
 
 const setUser = authId => axios
-  .post(`${process.env.HOST}/api/users/set`, { authId })
+  .post(`${process.env.HOST}/api/set/user`, { authId })
   .then(res => console.log('res.data: ', res.data))
   .catch(error => {
     // eslint-disable-next-line no-console
