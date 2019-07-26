df6abf37942e3e986d6bdf1600533d55f68e05a4
jerodb <jero.db@live.com>
2019-07-26 19:04:10 -0300

    add style login to header

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,10 +1,38 @@
+// import { logo } from '../../res/images'
+
 export default {
   container: {
     display: 'flex',
+    flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'center',
-    height: 80,
     borderBottom: '1px solid #aaaaaa',
-    backgroundColor: '#eeeeee'
+    backgroundColor: '#eeeeee',
+    paddingBottom: 16
+  },
+  topWrapper: {
+    display: 'flex',
+    flexDirection: 'row',
+    width: '100%'
+  },
+  logo: {
+    // background: `url(${logo}) no-repeat right top`,
+    backgroundSize: 'cover',
+    width: 200,
+    height: 58
+  },
+  nav: {
+    marginLeft: 'auto',
+    justifyContent: 'center',
+    display: 'flex',
+    flexDirection: 'column'
+  },
+  login: {
+    alignItems: 'flex-end',
+    justifyContent: 'center'
+  },
+  welcome: {
+    paddingTop: 72,
+    paddingBottom: 100
   }
 }
