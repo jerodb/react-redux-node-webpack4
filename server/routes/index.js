c0f5c2cbc8443224ebed332737246192c6acbc3a
jerodb <jero.db@live.com>
2019-07-26 19:00:35 -0300

    catch /auth/callback request

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,24 @@
 import express from 'express'
+import fs from 'fs'
+import path from 'path'
 import api from './api'
 import root from './root'
 
 const router = express.Router()
 
 router.use('/api', api)
+
+router.get('/auth/callback', (req, res) => {
+  const template = path.join(__dirname, '..', '..', 'dist', 'index.html')
+
+  // Loads template
+  fs.readFile(template, 'utf8', (err, document) => {
+    if (err) throw err
+
+    return res.send(document)
+  })
+})
+
 router.get('*', root)
 
 module.exports = router
