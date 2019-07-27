de8005cfd8046e2903d8570946e82590218896c9
jerodb <jero.db@live.com>
2019-07-27 06:47:49 -0300

    removed

--- src/lib/camelCased.js
+++
@@ -1,4 +0,0 @@
-/*
- ** Convert snake_case string to camelCase string
- */
-export default s => s.replace(/_([a-z])/g, g => g[1].toUpperCase())
--- src/lib/convertToFileName.js
+++
@@ -1,4 +0,0 @@
-export default name => name
-  .replace(' ', '-')
-  .replace('.', '')
-  .toLowerCase()
