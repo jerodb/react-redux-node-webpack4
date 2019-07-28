6a55c1cf6388b235bc56484d8a464c3646cab141
jerodb <jero.db@live.com>
2019-07-28 20:38:15 -0300

    no need to render css or sass in node

--- server/index.js
+++ server/index.js
@@ -1,13 +1,4 @@
 require('@babel/register')({
-  plugins: [
-    [
-      'css-modules-transform',
-      {
-        generateScopedName: '[hash:8]',
-        extensions: ['.css', '.scss'],
-      },
-    ],
-  ],
   presets: ['@babel/preset-env', '@babel/preset-react'],
 })
 
