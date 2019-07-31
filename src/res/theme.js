6d185050f1ed3d37134f92a08bc8f00ac8b7a5e8
jerodb <jero.db@live.com>
2019-07-31 13:43:25 -0300

    remove action button styles from theme

--- src/res/theme.js
+++ src/res/theme.js
@@ -3,15 +3,6 @@ import red from '@material-ui/core/colors/red'
 
 // Create theme instance.
 const theme = createMuiTheme({
-  actionBtn: {
-    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-    border: 0,
-    borderRadius: 3,
-    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-    color: 'white',
-    height: 48,
-    padding: '0 30px',
-  },
   mainPadding: {
     paddingBottom: 30,
     paddingLeft: 16,
--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -1,7 +1,15 @@
 import { makeStyles } from '@material-ui/styles'
 
-const styles = makeStyles(theme => ({
-  button: theme.actionBtn,
+const styles = makeStyles({
+  button: {
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    border: 0,
+    borderRadius: 3,
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    color: 'white',
+    height: 48,
+    padding: '0 30px',
+  },
   home: {
     minHeight: 300,
     textAlign: 'center',
@@ -10,6 +18,6 @@ const styles = makeStyles(theme => ({
     paddingTop: 24,
     paddingBottom: 24,
   },
-}))
+})
 
 export default styles
