d5bf218402ce1b0fd851e629f131ef5f809abb06
jerodb <jero.db@live.com>
2019-08-04 17:28:35 -0300

    add redux to SSR

--- server/routes/root.js
+++ server/routes/root.js
@@ -1,24 +1,30 @@
 import React from 'react'
 import { renderToString } from 'react-dom/server'
+import { Provider } from 'react-redux'
 import { StaticRouter } from 'react-router-dom'
 import { ServerStyleSheets, ThemeProvider } from '@material-ui/styles'
 import fs from 'fs'
 import path from 'path'
-import App from '../../src/App'
+import Router from '../../src/navigation/Router'
+import configureStore from '../../src/store'
 import theme from '../../src/res/theme'
 
 export default (req, res) => {
   // Create a sheetsManager instance.
   const sheets = new ServerStyleSheets()
 
+  const { store } = configureStore()
+
   // Render component to string.
   const markup = renderToString(
     sheets.collect(
-      <StaticRouter location={req.url} context={{}}>
-        <ThemeProvider theme={theme}>
-          <App />
-        </ThemeProvider>
-      </StaticRouter>,
+      <Provider store={store}>
+        <StaticRouter location={req.url} context={{}}>
+          <ThemeProvider theme={theme}>
+            <Router />
+          </ThemeProvider>
+        </StaticRouter>
+      </Provider>,
     )
   )
 
@@ -35,6 +41,7 @@ export default (req, res) => {
     const document = data
       .replace('<div id="app"></div>', `<div id="app">${markup}</div>`)
       .replace('<style id="jss-server-side"></style>', `<style id="jss-server-side">${styles}</style>`)
+      .replace('<script id="initial-state"></script>', `<script>window.INITIAL_STATE = ${JSON.stringify(store)}</script>`)
 
     // Sends html with the rendered React markup and styles.
     return res.send(document)
