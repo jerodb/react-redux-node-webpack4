0aff6881ca3ec85bca33e233790d88f4eb93f4ce
jerodb <jero.db@live.com>
2019-08-05 16:14:27 -0300

    Add PORT to config initialization

--- config.js
+++ config.js
@@ -4,13 +4,15 @@
 require('dotenv').config()
 
 const BASE_NAME = process.env.BASE_NAME || '/'
-const NODE_ENV = process.env.NODE_ENV || 'development'
 const HOST = process.env.HOST || 'http://localhost:3000/'
 const IMAGES_PATH = process.env.IMAGES_PATH || 'images/'
+const NODE_ENV = process.env.NODE_ENV || 'development'
+const PORT = process.env.PORT || 3000
 
 export {
   BASE_NAME,
-  NODE_ENV,
   HOST,
-  IMAGES_PATH
+  IMAGES_PATH,
+  NODE_ENV,
+  PORT,
 }
