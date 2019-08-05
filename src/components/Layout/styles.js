a8c16291a3a8025da9dbdd56f6c03529dbadf21f
jerodb <jero.db@live.com>
2019-08-05 03:50:21 -0300

    change in Layout styles

--- src/components/Layout/styles.js
+++ src/components/Layout/styles.js
@@ -2,6 +2,9 @@ import { makeStyles } from '@material-ui/styles'
 
 const styles = makeStyles(theme => ({
   container: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
     ...theme.mainPadding
   },
 }))
