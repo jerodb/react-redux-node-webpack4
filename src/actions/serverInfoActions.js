a60fbe0728059f16f1f97dd6100fed101e51efb6
jerodb <jero.db@live.com>
2019-08-05 03:50:05 -0300

    Add new serverInfo types and actions

--- src/actions/serverInfoActions.js
+++ src/actions/serverInfoActions.js
@@ -1,5 +1,11 @@
-import { SERVER_INFO_IS_FETCHING, SERVER_INFO_SET } from './_types'
+import {
+  SERVER_INFO_CLEAR, SERVER_INFO_ERROR, SERVER_INFO_IS_FETCHING, SERVER_INFO_SET
+} from './_types'
+
+export const clearServerInfo = () => ({ type: SERVER_INFO_CLEAR })
 
 export const getServerInfo = () => ({ type: SERVER_INFO_IS_FETCHING })
 
-export const setServerInfo = serverInfo => ({ type: SERVER_INFO_SET, serverInfo })
+export const setServerInfo = serverInfo => ({ type: SERVER_INFO_SET, data: serverInfo })
+
+export const showError = error => ({ type: SERVER_INFO_ERROR, error })
--- src/reducers/serverInfoReducer.js
+++ src/reducers/serverInfoReducer.js
@@ -1,23 +1,36 @@
-import { SERVER_INFO_IS_FETCHING, SERVER_INFO_SET } from '../actions/_types'
+import {
+  SERVER_INFO_CLEAR, SERVER_INFO_ERROR, SERVER_INFO_IS_FETCHING, SERVER_INFO_SET
+} from '../actions/_types'
 
 const initialState = {
+  data: null,
+  error: '',
   isFetching: false,
-  errorMessage: '',
-  serverInfo: null,
 }
 
 export const serverInfo = (state = initialState, action) => {
   switch (action.type) {
+    case SERVER_INFO_CLEAR:
+      return {
+        ...initialState
+      }
+    case SERVER_INFO_ERROR:
+      return {
+        data: null,
+        error: action.error,
+        isFetching: false,
+      }
     case SERVER_INFO_IS_FETCHING:
       return {
-        ...initialState,
+        data: null,
+        error: '',
         isFetching: true
       }
     case SERVER_INFO_SET:
       return {
-        ...initialState,
-        serverInfo: action.serverInfo
-
+        data: action.data,
+        error: '',
+        isFetching: false,
       }
     default:
       return state
