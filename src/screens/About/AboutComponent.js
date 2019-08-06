3b86515632e32aa561140520cd11c37aeb809292
jerodb <jero.db@live.com>
2019-08-06 18:18:53 -0300

    update About text

--- src/screens/About/AboutComponent.js
+++ src/screens/About/AboutComponent.js
@@ -7,9 +7,8 @@ const AboutComponent = () => {
 
   return (
     <div className={classes.about}>
-      <p>This is a SPA skeleton builed using React, Redux, Webpack4, Material-UI and node w/express for the server.</p>
+      <p>This is an SPA skeleton built using React, Redux, Webpack4, Material-UI and node w/express for the server.</p>
       <p>SSR includes material-ui styles and redux initial state.</p>
-      <p>Authentication using Auth0 is integrated and can be used optionally.</p>
     </div>
   )
 }
