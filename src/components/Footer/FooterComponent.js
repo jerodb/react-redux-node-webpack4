5154d5efd0160ae26f80de0b3944c309162900d8
jerodb <jero.db@live.com>
2019-08-06 18:18:12 -0300

    change footer styles

--- src/components/Footer/FooterComponent.js
+++ src/components/Footer/FooterComponent.js
@@ -1,12 +1,19 @@
 import React from 'react'
+import { version } from '../../../package.json'
 import styles from './styles'
 
 const Footer = () => {
   const classes = styles()
 
   return (
-    <div className={classes.container}>
-      Maybe a footer here
+    <div className={classes.wrapper}>
+      <div className={classes.container}>
+        <span>
+          {`Currently v.${version}. Released under the`}
+          &nbsp;
+        </span>
+        <a href="https://github.com/jerodb/react-redux-node-webpack4/LICENCE">MIT License.</a>
+      </div>
     </div>
   )
 }
--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -1,14 +1,25 @@
 import { makeStyles } from '@material-ui/styles'
 
-const styles = makeStyles({
+const styles = makeStyles(theme => ({
   container: {
-    display: 'flex',
+    ...theme.mainContainer,
     alignItems: 'center',
-    justifyContent: 'center',
-    height: 120,
-    borderTop: '1px solid #aaaaaa',
-    backgroundColor: '#eeeeee'
-  }
-})
+    paddingTop: 16,
+    paddingBottom: 16,
+    '& a': {
+      whiteSpace: 'nowrap',
+    },
+    flexDirection: 'column',
+    [theme.breakpoints.up(360)]: {
+      flexDirection: 'row',
+    },
+  },
+  wrapper: {
+    backgroundColor: '#FAFAFA',
+    borderBottom: '1px solid #EAEAEA',
+    borderTop: '1px solid #EAEAEA',
+    width: '100%',
+  },
+}))
 
 export default styles
