d31978dd9e4203f70214b3aad1323840ec9e2fb5
jerodb <jero.db@live.com>
2019-08-14 04:29:25 -0300

    update actions

--- src/actions/serverInfoActions.js
+++ src/actions/serverInfoActions.js
@@ -1,11 +1,18 @@
 import {
-  SERVER_INFO_CLEAR, SERVER_INFO_ERROR, SERVER_INFO_IS_FETCHING, SERVER_INFO_SET
+  SERVER_INFO_ERROR, SERVER_INFO_IS_FETCHING, SERVER_INFO_SET
 } from './_types'
+import { getServerInfo as getInfo } from '../services'
 
-export const clearServerInfo = () => ({ type: SERVER_INFO_CLEAR })
-
-export const getServerInfo = () => ({ type: SERVER_INFO_IS_FETCHING })
-
-export const setServerInfo = serverInfo => ({ type: SERVER_INFO_SET, data: serverInfo })
-
+export const setServerInfo = data => ({ type: SERVER_INFO_SET, data })
 export const showError = error => ({ type: SERVER_INFO_ERROR, error })
+export const startFetchingInfo = () => ({ type: SERVER_INFO_IS_FETCHING })
+
+// redux-thunk action creator
+export const getServerInfo = () => async dispatch => {
+  dispatch(startFetchingInfo())
+
+  getInfo().then(data => {
+    if (data && data.error) return dispatch(showError(data.error))
+    return dispatch(setServerInfo(data))
+  })
+}
