52687d70f0a5d70bc2658a98272b826ea37d3b9c
jerodb <jero.db@live.com>
2019-08-14 04:40:49 -0300

    create serverInfo mock

---
+++ test/__mocks__/data.js
@@ -0,0 +1,22 @@
+export const serverInfo = [
+  {
+    name: 'arch',
+    value: 'x64',
+  },
+  {
+    name: 'cpus',
+    value: '4 cpus Intel(R) Core(TM) i5-4278U CPU @ 2.60GHz',
+  },
+  {
+    name: 'memory',
+    value: '8 GB',
+  },
+  {
+    name: 'hostname',
+    value: 'Some hostname',
+  },
+  {
+    name: 'platform',
+    value: 'darwin',
+  },
+]
