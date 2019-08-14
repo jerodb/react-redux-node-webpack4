1e8b6a30a7a6da95e263c3fd78bdbe4c9920f7dd
jerodb <jero.db@live.com>
2019-08-14 04:29:58 -0300

    change texts

--- src/screens/About/About.test.js
+++ src/screens/About/About.test.js
@@ -2,8 +2,8 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import About from './About.component'
 
-describe('About Component', () => {
-  it('Should render without errors', () => {
+describe('About Component unit tests', () => {
+  it('Render without errors', () => {
     shallow(<About />)
   })
 })
