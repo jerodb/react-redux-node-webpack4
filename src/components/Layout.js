710d6517e13770de32728734cbe18e9259d20519
jerodb <jero.db@live.com>
2019-10-02 15:16:36 -0300

    simplify Layout component

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,28 +1,13 @@
 import React from 'react'
-import { makeStyles } from '@material-ui/styles'
 import Header from './Header'
 import Footer from './Footer'
 
-const Layout = ({ Comp }) => {
-  const classes = styles()
-
-  return (
-    <>
-      <Header />
-      <div className={classes.container}>
-        <Comp />
-      </div>
-      <Footer />
-    </>
-  )
-}
+const Layout = ({ Comp }) => (
+  <>
+    <Header />
+    <Comp />
+    <Footer />
+  </>
+)
 
 export default Layout
-
-const styles = makeStyles(theme => ({
-  container: {
-    paddingBottom: 30,
-    paddingTop: 30,
-    ...theme.mainContainer,
-  },
-}))
